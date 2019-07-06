94c702200125b1dc4847f28ccf8637bbe5dc2ec9
AAYUSH MITTAL <aayushmittal1998@gmail.com>
2019-07-06 14:37:09 +0530

    Add files via upload

---
+++ build/precache-manifest.09778298b4c70ae00de82cce3777dec6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1153359033c35ee676a6332877a00b9",
+    "url": "/ui/index.html"
+  },
+  {
+    "revision": "bad184e4357a919f9b07",
+    "url": "/ui/static/css/main.00aab312.chunk.css"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/ui/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "bad184e4357a919f9b07",
+    "url": "/ui/static/js/main.905bebc6.chunk.js"
+  },
+  {
+    "revision": "c6bf5e5406f5c94d3a42",
+    "url": "/ui/static/js/runtime~main.13b8cc1c.js"
+  }
+]);
